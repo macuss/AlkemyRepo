63e125f563c8fea0901ebc3286db0dd5db326bfd
Maximiliano Schultz <macuss@gmail.com>
2023-12-27 16:49:16 -0300

    agregando cambios en la nuevaRama

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// este es un cambio en la nueva rama
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
